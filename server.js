47d816ee566a1936674ea46f3dae92d2cb14aa1b
mg-skc <71731493+mg-skc@users.noreply.github.com>
2021-01-15 11:43:29 -0600

    ejs formatting - page templates

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const { appendFileSync } = require('fs')
 const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+
+
+const initializePassport = require('./passport-config')
+initializePassport(passport)
+
+//THIS WILL NEED TO BE CHANGED TO POSTS WITH DATABASE STORAGE FOR PRODUCTION!!!
+const users = []
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -12,13 +21,31 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+app.post('/login', (req, res) => {
+   
+})
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register'), (req, res) =>{
-    
+app.post('/register', async (req, res) =>{
+try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+        //this ID will be auto generated when connected to database
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+
+    })
+    res.redirect('/login')
+} catch{
+    res.redirect('/register')
 }
+console.log(users)
+})
 
 app.listen(3000)
 
