545f2a6168a1541c6e470228ac0d13fc7178affb
mg-skc <71731493+mg-skc@users.noreply.github.com>
2021-01-19 13:24:21 -0600

    Finishing the code. ignore emmet test

--- passport-config.js
+++ passport-config.js
@@ -1,11 +1,30 @@
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require ('bcrypt')
 
-function initialize(passport) {
-    const authenticateUser = (email, password, done) => {
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email)
+        if (user == null) {
+            return done(null, false, {message: 'No user with that email'})
+        }
+    try {
+        if (await bcrypt.compare(password, user.password)) {
+            return done(null, user)
+        } else {
+            return done(null, false, {message: 'Password incorrect'})
+        }
+        } catch (e) {
+            return done(e)
+      }
+    }
 
+    passport.use(new LocalStrategy({usernameField: 'email'}, 
+    authenticateUser))
+    //Need to change this to the real ID we'll be using...likely email or auto assigned ID
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
     }
-    passport.use(new LocalStrategy({usernameField: 'email'}), 
-    authenticateUser)
-    passport.serializeUser((user, done) => {})
-    passport.deserializeUser((id, done) => {})
-    }
+    )}
+
+    module.exports = initialize
